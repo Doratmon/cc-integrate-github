54aba77afd0f8e99f6586547dca2a201de49189d
leanwe_mac <maleanwei@gmail.com>
2025-12-28 21:40:54 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { getUser } from "@/actions";
+import { getProjects } from "@/actions/get-projects";
+import { createProject } from "@/actions/create-project";
+import { MainContent } from "./main-content";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const user = await getUser();
+
+  // If user is authenticated, redirect to their most recent project
+  if (user) {
+    const projects = await getProjects();
+    
+    if (projects.length > 0) {
+      redirect(`/${projects[0].id}`);
+    }
+
+    // If no projects exist, create a new one
+    const newProject = await createProject({
+      name: `New Design #${~~(Math.random() * 100000)}`,
+      messages: [],
+      data: {},
+    });
+
+    redirect(`/${newProject.id}`);
+  }
+
+  // For anonymous users, show the main content without a project
+  return <MainContent user={user} />;
+}
